650b0e731ab9ef06aa4c743669c45dfce2619146
diogojorge <diogo.jorge97@hotmail.com>
2019-01-05 21:10:26 +0000

    Calculator done

--- app/src/main/java/pt/ua/icm/studentmanagerv1/ListCalculator.java
+++ app/src/main/java/pt/ua/icm/studentmanagerv1/ListCalculator.java
@@ -16,6 +16,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -35,44 +37,44 @@ public class ListCalculator extends AppCompatActivity {
         setContentView(R.layout.activity_list_calculator);
 
         Intent intent = getIntent();
-        Map<String, List<String>> calculatorEvaluations = (Map<String, List<String>>)intent.getSerializableExtra(ListIndividualCourse.EXTRA_EVALUATIONS);
+        Map<String, List<String>> calculatorEvaluations = (Map<String, List<String>>) intent.getSerializableExtra(ListIndividualCourse.EXTRA_EVALUATIONS);
         Log.d(TAG, "Map: " + calculatorEvaluations.toString());
 
+        isEnabled = 4;
         main_lock = findViewById(R.id.main_lock);
         main_grade = findViewById(R.id.main_grade);
 
         showEvaluations(calculatorEvaluations);
 
         main_grade.setEnabled(false);
+        isEnabled--;
 
         main_lock.setOnClickListener(view1 -> {
-            Log.d(TAG, main_grade.getKeyListener().toString());
-            if (main_grade.isEnabled()){
+            if (main_grade.isEnabled()) {
                 main_grade.setEnabled(false);
+                isEnabled--;
                 main_lock.setImageResource(R.drawable.ic_lock);
             } else {
                 main_grade.setEnabled(true);
                 main_lock.setImageResource(R.drawable.ic_unlock);
+                isEnabled++;
 
             }
         });
 
 
-
-
-
     }
 
-    private void showEvaluations(Map<String,List<String>> calculatorEvaluations) {
+    private void showEvaluations(Map<String, List<String>> calculatorEvaluations) {
         LinearLayout parent = findViewById(R.id.list_all_calculator);
         editTextList = new HashMap<>();
 
-        String name = "";
-        String percentage = "";
-        String grade = "";
+        String name;
+        String percentage;
+        String grade;
 
 
-        for (Map.Entry entry: calculatorEvaluations.entrySet()){
+        for (Map.Entry entry : calculatorEvaluations.entrySet()) {
 
             LayoutInflater factory = LayoutInflater.from(this);
             View view = factory.inflate(R.layout.list_individual_calculator, null);
@@ -90,54 +92,42 @@ public class ListCalculator extends AppCompatActivity {
             percentage = values.get(0);
             grade = values.get(1);
 
-            editTextList.put(gradeEV, percentage.replace("%",""));
+            editTextList.put(gradeEV, percentage.replace("%", ""));
 
 
             nameTV.setText(name);
             percentageTV.setText(percentage);
-            if (!grade.equals("0")){
+            if (!grade.equals("0")) {
                 gradeEV.setText(grade);
                 gradeEV.setEnabled(false);
+                isEnabled--;
                 unLockIV.setImageResource(R.drawable.ic_lock);
             }
 
             editTextList.put(main_grade, "100");
             unLockIV.setOnClickListener(view1 -> {
-                isEnabled = 0;
 
-                if (gradeEV.isEnabled()){
+                if (gradeEV.isEnabled()) {
                     gradeEV.setEnabled(false);
+                    isEnabled--;
                     unLockIV.setImageResource(R.drawable.ic_lock);
                 } else {
                     gradeEV.setEnabled(true);
+                    isEnabled++;
                     unLockIV.setImageResource(R.drawable.ic_unlock);
                 }
-
-                for (EditText editText: editTextList.keySet()){
-                    if (editText.isEnabled()){
-                        isEnabled++;
-                    }
-                }
-/*                if (isEnabled!=1){
-                    main_grade.setEnabled(false);
-                } else {
-                    main_grade.setEnabled(true);
-                }*/
-
-
             });
             parent.addView(view);
 
 
-            for (EditText editText: editTextList.keySet()){
+            for (EditText editText : editTextList.keySet()) {
                 editText.setOnEditorActionListener((textView, i, keyEvent) -> {
-                    Log.d(TAG, "I: " + i);
                     if ((keyEvent != null && (keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (i == EditorInfo.IME_ACTION_DONE)) {
-                        //Log.d(TAG, "" + keyEvent.getKeyCode());
-                        if (isEnabled==2){
+                        if (isEnabled == 2) {
                             calculateGrade(editText);
                         } else {
-                            Toast.makeText(this, "Desbloqueie apenas dois cadeados para se poder efectuar os calculos", Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "Enabled: " + isEnabled);
+                            Toast.makeText(this, R.string.lock_error_message, Toast.LENGTH_LONG).show();
                         }
                     }
                     return false;
@@ -147,68 +137,79 @@ public class ListCalculator extends AppCompatActivity {
         main_grade.setEnabled(false);
 
 
-
-
-
-
-
     }
 
     private void calculateGrade(EditText pressedEditText) {
         double varGrade = 0.0;
         EditText varEditText = null;
         double varPercentage = 100.0;
+
         double mainGrade = 0.0;
         List<Double> fixedGrades = new ArrayList<>();
-        for (Map.Entry editTextEntryMap: editTextList.entrySet()){
+        for (Map.Entry editTextEntryMap : editTextList.entrySet()) {
             EditText editText = (EditText) editTextEntryMap.getKey();
-            String editTxGrade = editText.getText().toString();
-            if (editTxGrade.isEmpty()){
-                editTxGrade = "0";
+            String grade = editText.getText().toString();
+            String percentage = editTextEntryMap.getValue().toString();
+            if (grade.isEmpty()) {
+                grade = "0";
             }
-            if (Double.parseDouble(editTextEntryMap.getValue().toString())==100){
-                mainGrade = Double.parseDouble(editTxGrade);
+
+            if (Double.parseDouble(percentage) == 100) {
+                mainGrade = Double.parseDouble(grade);
+                if (editText.isEnabled() && !editText.equals(pressedEditText)) {
+                    varEditText = editText;
+                    varPercentage = Double.parseDouble(percentage) / 100;
+                }
                 continue;
             }
-            if (editText.isEnabled()){
-                if (editText.equals(pressedEditText)){
-                    fixedGrades.add(Double.parseDouble(editTxGrade) * Double.parseDouble(editTextEntryMap.getValue().toString())/100);
+            if (editText.isEnabled()) {
+                if (editText.equals(pressedEditText)) {
+                    fixedGrades.add(Double.parseDouble(grade) * Double.parseDouble(percentage) / 100);
                 } else {
-                    varGrade = Double.parseDouble(editTxGrade) * Double.parseDouble(editTextEntryMap.getValue().toString())/100;
+                    varGrade = Double.parseDouble(grade) * Double.parseDouble(percentage) / 100;
                     varEditText = editText;
-                    varPercentage = Double.parseDouble(editTextEntryMap.getValue().toString())/100;
+                    varPercentage = Double.parseDouble(percentage) / 100;
                 }
             } else {
-                fixedGrades.add(Double.parseDouble(editTxGrade) * Double.parseDouble(editTextEntryMap.getValue().toString())/100);
+                if (grade.isEmpty()) {
+                    Toast.makeText(this, R.string.lock_error_message2, Toast.LENGTH_SHORT).show();
+                    break;
+                }
+                fixedGrades.add(Double.parseDouble(grade) * Double.parseDouble(percentage) / 100);
             }
 
 
         }
 
 
-        if (varGrade == 0.0){
-            varGrade = mainGrade;
-            varEditText = findViewById(R.id.main_grade);
-        }
+        Double value;
 
-
-
-        Log.d(TAG, "Fixed: " + fixedGrades.toString());
-        Log.d(TAG, "Varia: " + varGrade);
-        Log.d(TAG, "Main: " + mainGrade);
-
-        Double fixedTotal= 0.0;
-        for (Double elem: fixedGrades){
+        Double fixedTotal = 0.0;
+        for (Double elem : fixedGrades) {
             fixedTotal += elem;
         }
 
-        Double value = mainGrade - fixedTotal;
-        value = value / varPercentage;
-        Log.d(TAG, "VarEditID: " + varEditText.getId());
-        varEditText.setText(value.toString());
+        if (varPercentage == 1) {
+            varGrade = mainGrade;
+            varEditText = findViewById(R.id.main_grade);
+            value = fixedTotal;
+        } else {
+            value = mainGrade - fixedTotal;
+            value = value / varPercentage;
+        }
 
-    }
 
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        varEditText.setText(df.format(value));
+
+        Log.d(TAG,"Fixed: "+fixedGrades.toString());
+        Log.d(TAG,"Varia: "+varGrade);
+        Log.d(TAG,"Main: "+mainGrade);
+
+
+}
 
 
 }
