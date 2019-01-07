6e37cb25cd5b47ae0c4563c47b9b230a2e3ee8f3
joaosilva9 <joaosilva9@ua.pt>
2019-01-07 02:09:16 +0000

    Rewrite CalendarFragment

--- app/src/main/java/pt/ua/icm/studentmanagerv1/CalendarFragment.java
+++ app/src/main/java/pt/ua/icm/studentmanagerv1/CalendarFragment.java
@@ -5,6 +5,7 @@ import android.accounts.Account;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -31,7 +32,7 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment {
 
     public static final String ACCOUNT_NAME = "Local Calendar";
 
@@ -39,12 +40,7 @@ public class CalendarFragment extends Fragment {
 
     private static final String INT_NAME_PREFIX = "local_";
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //View view = inflater.inflate(R.layout.calendar_fragment, container, false);
-
-        createCalendar("testes", 0xffff0000);
-        createCalendar("estudo", 0x0000ffff);
+    public void mainCalendar(Context context) {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         long startMillis = timestamp.getTime();
 
@@ -53,7 +49,7 @@ public class CalendarFragment extends Fragment {
         ContentUris.appendId(builder, startMillis);
         Intent intent = new Intent(Intent.ACTION_VIEW)
                 .setData(builder.build());
-        startActivity(intent);
+        context.startActivity(intent);
 
         String today = "07/01/2019";
 
@@ -76,42 +72,46 @@ public class CalendarFragment extends Fragment {
         }
 
 
-        AllMightyCreator.getDb().document("/Students/St" + AllMightyCreator.getnMec() +"/Courses/2018-2019#Primeiro#45424/Evaluations/Discreet Evaluation").get()
+        AllMightyCreator.getDb().document("/Students/St" + AllMightyCreator.getnMec() + "/Courses/2018-2019#Primeiro#45424/Evaluations/Discreet Evaluation").get()
                 .addOnSuccessListener(documentSnapshot -> {
 
                     Map<String, Object> data = documentSnapshot.getData();
-                    for ( String key : data.keySet() ) {
-                        if (data.get(key) instanceof HashMap){
+                    for (String key : data.keySet()) {
+                        if (data.get(key) instanceof HashMap) {
                             Object camp = data.get(key);
-                            for (Object key1 : ((HashMap) camp).values()){
+                            for (Object key1 : ((HashMap) camp).values()) {
                                 SimpleDateFormat sdf3 = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
                                 Date d1 = null;
-                                try{
+                                try {
                                     d1 = sdf3.parse(String.valueOf(((HashMap) key1).get("Date")));
 
-                                }catch (Exception e){ e.printStackTrace(); }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
                                 HashMap ok = (HashMap) key1;
                                 long beginTime = d1.getTime();
-                                long endTime  = d1.getTime();
+                                long endTime = d1.getTime();
                                 String title = String.valueOf(((HashMap) key1).get("Name"));
-                                insertEvent(1, beginTime, endTime, title, "testes");
-                                for (Object x : ok.keySet()){
-                                    if (x.toString().equals("Study Sessions")){
+                                insertEvent(1, beginTime, endTime, title, context);
+                                for (Object x : ok.keySet()) {
+                                    if (x.toString().equals("Study Sessions")) {
                                         HashMap dates = (HashMap) ok.get(x);
-                                        for (Object y : dates.values()){
+                                        for (Object y : dates.values()) {
                                             int cont = 0;
-                                            for (Object z : (ArrayList) y){
+                                            for (Object z : (ArrayList) y) {
                                                 StringBuilder title1 = new StringBuilder(title + " - StudySessions ");
                                                 Date d2 = null;
-                                                try{
+                                                try {
                                                     d2 = sdf3.parse(String.valueOf(z));
 
-                                                }catch (Exception e){ e.printStackTrace(); }
+                                                } catch (Exception e) {
+                                                    e.printStackTrace();
+                                                }
                                                 long beginTimeSession = d2.getTime();
-                                                long endTimeSession  = d2.getTime();
+                                                long endTimeSession = d2.getTime();
                                                 title1.append(cont);
-                                                insertEvent(1, beginTimeSession, endTimeSession, title1.toString(), "estudo");
-                                                cont+=1;
+                                                insertEvent(1, beginTimeSession, endTimeSession, title1.toString(), context);
+                                                cont += 1;
                                             }
 
                                         }
@@ -124,51 +124,19 @@ public class CalendarFragment extends Fragment {
                 }).addOnFailureListener(e -> {
         });
 
-        /*for (Map.Entry<String, HashMap<String, Object>> val : infos.entrySet()){
-            Log.w("WARNING", val.getKey());
-        }*/
 
-        return null;
     }
 
-    private void createCalendar(String displayName, int color) {
-        Uri uri = CalendarContract.Calendars.CONTENT_URI.buildUpon()
-                .appendQueryParameter(CalendarContract.CALLER_IS_SYNCADAPTER, "true")
-                .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_NAME, ACCOUNT_NAME)
-                .appendQueryParameter(CalendarContract.Calendars.ACCOUNT_TYPE, ACCOUNT_TYPE).build();
 
-        String intName = INT_NAME_PREFIX + displayName;
-        ContentValues cv = new ContentValues();
-        cv.put(CalendarContract.Calendars.ACCOUNT_NAME, ACCOUNT_NAME);
-        cv.put(CalendarContract.Calendars.ACCOUNT_TYPE, ACCOUNT_TYPE);
-        cv.put(CalendarContract.Calendars.NAME, intName);
-        cv.put(CalendarContract.Calendars.CALENDAR_DISPLAY_NAME, displayName);
-        cv.put(CalendarContract.Calendars.CALENDAR_COLOR, color);
-        cv.put(CalendarContract.Calendars.CALENDAR_ACCESS_LEVEL, CalendarContract.Calendars.CAL_ACCESS_OWNER);
-        cv.put(CalendarContract.Calendars.OWNER_ACCOUNT, ACCOUNT_NAME);
-        cv.put(CalendarContract.Calendars.VISIBLE, 1);
-        cv.put(CalendarContract.Calendars.SYNC_EVENTS, 1);
-
-        ContentResolver cr = getActivity().getContentResolver();
-        cr.insert(uri, cv);
-
-        /**Intent main = new Intent();
-        main.putExtra("useurl", resultUri);
-        Intent intent = new Intent(Intent.ACTION_VIEW)
-                .setData(resultUri);
-        startActivity(intent);**/
-    }
-
-    public void insertEvent(long calID, long beginTime, long endTime, String title, String nameCalendar) {
-        ContentResolver cr = getActivity().getContentResolver();
+    public void insertEvent(long calID, long beginTime, long endTime, String title, Context context) {
+        ContentResolver cr = context.getContentResolver();
         ContentValues values = new ContentValues();
         values.put(CalendarContract.Events.DTSTART, beginTime);
         values.put(CalendarContract.Events.DTEND, endTime);
         values.put(CalendarContract.Events.TITLE, title);
         values.put(CalendarContract.Events.CALENDAR_ID, calID);
         values.put(CalendarContract.Events.EVENT_TIMEZONE, "Portugal/Lisbon");
-        //values.put(CalendarContract.Calendars.NAME, nameCalendar);
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
